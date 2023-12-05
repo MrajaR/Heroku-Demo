154e0495f837a7fc8512e67465ca6aa2e6a2c0ac
Raja Reivan <122173033+MrajaR@users.noreply.github.com>
2023-12-05 15:39:40 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    int_features = [np.array(int_features)]
+    prediction = model.predict(int_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Gaji karyawan berkisar pada $ {}'.format(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
